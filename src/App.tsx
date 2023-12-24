d519c745af10294ed8d623c6bf8ff3dca82f2f33
mustafiz-codes <js.mustafiz@gmail.com>
2023-12-24 13:55:30 +0000

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Weekly from "./views/weekly";
+import Monthly from "./views/monthly";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Weekly />} />
+      <Route path="/monthly" element={<Monthly />} />
+    </Routes>
   );
 }
-
 export default App;
---
+++ src/views/monthly.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Monthly = () => {
+  return (
+    <div>
+      <h2>THIS IS MONTHLY PAGE</h2>
+    </div>
+  );
+};
+
+export default Monthly;
